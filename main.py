0e4932aae347ec9c1c44616fdd024f1a49f8ffac
pardo2410 <pardo2410@gmail.com>
2020-06-10 00:51:49 +0200

    Game over

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
   
 import pygame as pg
 from pygame.locals import *
-import sys
+import sys, random
 
-BACKGROUND = (0,240,0)
-YELLOW = (255, 255, 0)
+BACKGROUND = (50,50,50)
+YELLOW = (255, 255, 0)  
+WHITE = (255, 255, 255)
+
+WIN_GAME_SCORE = 3
+
 class Ball: 
     def __init__(self):
-        self.vx = 5
-        self.vy = 5
-        self.Cx = 400
-        self.Cy = 300
+        self.reset()
         self.h = 20
         self.w = 20
 
         self.image = pg.Surface((self.w, self.h))
         self.image.fill(YELLOW)
+        self.ping = pg.mixer.Sound('./resources/sounds/ping.wav')
+        self.lost_point = pg.mixer.Sound('./resources/sounds/lost-point.wav')
 
     @property
     def posx(self):
@@ -27,22 +30,40 @@ class Ball:
 
     def move(self, limSupX, limSupY):
         if self.Cx >= limSupX or self.Cx <=0:
-            self.vx *= -1
+            self.vx = 0
+            self.vy = 0
+            self.lost_point.play()
 
         if self.Cy >= limSupY or self.Cy <=0:
             self.vy *= -1
+            self.ping.play()
                 
         self.Cx += self.vx
         self.Cy += self.vy
 
+    def comprobarChoque(self, something):
+        dx = abs(self.Cx - something.Cx)
+        dy = abs(self.Cy - something.Cy)
+
+        if dx < (self.w + something.w)//2 and dy < (self.h +something.h) // 2:
+            self.vx *= -1
+            self.Cx += self.vx
+            self.Cy += self.vy
+            self.ping.play()
+
+    def reset(self):
+        self.vx = random.choice([-5, -3, 3, 5])
+        self.vy = random.choice([-5, -3, 3, 5]) 
+        self.Cx = 400
+        self.Cy = 300
 
 class Raquet:
-    def __init__(self):
+    def __init__(self, Cx):
         self.vx = 0
-        self.vy = 5
+        self.vy = 0
         self.w = 25
         self.h = 100
-        self.Cx = 30
+        self.Cx = Cx
         self.Cy = 300
 
         self.image = pg.Surface((self.w, self.h))
@@ -56,11 +77,16 @@ class Raquet:
     def posy(self):
         return self.Cy - self.h // 2
 
-    def moveR(self,limSupY):
+    def move(self, limSupX, limSupY):
+        self.Cx += self.vx
+        self.Cy += self.vy
 
-        if self.Cy >= limSupY or self.Cy <=0:
-            self.vy *= -1
-    
+        if self.Cy < self.h //2:
+            self.Cy = self.h // 2
+
+        if self.Cy > limSupY - self.h // 2:
+            self.Cy = limSupY - self.h // 2
+
         
 
 class Game:
@@ -69,38 +95,121 @@ class Game:
         self.pantalla.fill(BACKGROUND)
         self.fondo = pg.image.load("./resources/images/fondo.jpg")
         self.ball = Ball()
-        self.playerOne = Raquet()
+        self.playerOne = Raquet(30)
+        self.playerTwo = Raquet(770)
+
+        self.status = 'Partida'
+
+        self.font = pg.font.Font('./resources/fonts/PressStart2P-Regular.ttf', 40)
+        self.fontGrande = pg.font.Font('./resources/fonts/PressStart2P-Regular.ttf', 60)
+
+        self.marcadorOne = self.font.render("0", True, WHITE)
+        self.marcadorTwo = self.font.render("0", True, WHITE)
+
+        self.text_game_over = self.fontGrande.render("GAME OVER", True, YELLOW)
+        self.text_insert_coin = self.font.render('<SPACE>-Inicio partida', True, WHITE)
+
+        self.scoreOne = 0
+        self.scoreTwo = 0
         pg.display.set_caption("Pong")
 
-    def main_loop(self):
+
+    def handlenEvent(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                self.quit()
+            '''
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    self.playerOne.vy = -5
+                if event.key == K_DOWN:
+                    self.playerOne.vy = 5
+            '''
+        key_pressed = pg.key.get_pressed()
+        if key_pressed[K_UP]:
+            self.playerTwo.vy = - 5
+        elif key_pressed[K_DOWN]:
+            self.playerTwo.vy = 5
+        else:
+            self.playerTwo.vy = 0
+
+        if key_pressed[K_w]:
+            self.playerOne.vy = - 5
+        elif key_pressed[K_s]:
+            self.playerOne.vy = 5
+        else:
+            self.playerOne.vy = 0
+        
+        return False
+
+    def bucle_partida(self):
         game_over = False
+        self.scoreOne = 0
+        self.scoreTwo = 0
+        self.marcadorOne = self.font.render(str(self.scoreOne), True, WHITE)
+        self.marcadorTwo = self.font.render(str(self.scoreOne), True, WHITE)
 
         while not game_over:
-            for event in pg.event.get():
-                if event.type == QUIT:
+            game_over = self.handlenEvent()
+
+            self.ball.move(800, 600)
+            self.playerOne.move(800, 600)
+            self.playerTwo.move(800, 600)
+            self.ball.comprobarChoque(self.playerOne)
+            self.ball.comprobarChoque(self.playerTwo)
+
+            if self.ball.vx == 0 and self.ball.vy == 0:
+                if self.ball.Cx >=800:
+                    self.scoreOne += 1
+                    self.marcadorOne = self.font.render(str(self.scoreOne), True, WHITE)
+                if self.ball.Cx <= 0:
+                    self.scoreTwo += 1
+                    self.marcadorTwo = self.font.render(str(self.scoreTwo), True, WHITE)
+
+                if self.scoreOne == WIN_GAME_SCORE or self.scoreTwo == WIN_GAME_SCORE:
                     game_over = True
 
-                if event.type == KEYDOWN:
-                    if event.key == K_w:
-                        self.playerOne.Cy -= self.playerOne.vy
-
-                    if event.key == K_z:
-                        self.playerOne.Cy += self.playerOne.vy
-
-            key_pressed = pg.key.get_pressed()
-            if key_pressed[K_w]:
-                self.playerOne.Cy -= self.playerOne.vy
-            if key_pressed[K_z]:
-                self.playerOne.Cy += self.playerOne.vy
+                self.ball.reset()
 
             self.pantalla.blit(self.fondo, (0, 0))
             self.pantalla.blit(self.ball.image, (self.ball.posx, self.ball.posy))
             self.pantalla.blit(self.playerOne.image, (self.playerOne.posx, self.playerOne.posy))
-
-            self.ball.move(800, 600)
-            self.playerOne.moveR(600)
+            self.pantalla.blit(self.playerTwo.image, (self.playerTwo.posx, self.playerTwo.posy))
+            self.pantalla.blit(self.marcadorOne, (30, 10))
+            self.pantalla.blit(self.marcadorTwo, (740, 10))
 
             pg.display.flip()
+
+        self.status = 'Inicio'
+
+    def bucle_inicio(self):
+        inicio_partida = False
+        while not inicio_partida:
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    self.quit()
+
+                if event.type == KEYDOWN:
+                    if event.key == K_SPACE:
+                        inicio_partida = True
+
+            self.pantalla.fill((0,0, 255))
+            self.pantalla.blit(self.text_game_over, (100, 100))
+            self.pantalla.blit(self.text_insert_coin, (100, 200))     
+
+            pg.display.flip()       
+
+        self.status = 'Partida'
+
+
+    def main_loop(self):
+
+        while True:
+            if self.status == 'Partida':
+                self.bucle_partida()
+            else:
+                self.bucle_inicio()
+
 
     def quit(self):
         pg.quit()
